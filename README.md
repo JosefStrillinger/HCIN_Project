7e3fdc10391a8e74cdad8dec10f5868370da1b3f
Josef Strillinger <jstrillinger@outlook.com>
2023-03-09 15:58:01 +0100

    v1.0

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # HCIN_Project
+
 Project for HCIN by Maxi Pumpfer and Josef Strillinger
---
+++ gtts_play_sound.py
@@ -0,0 +1,14 @@
+from gtts import gTTS
+from io import BytesIO
+from pydub import AudioSegment
+from pydub.playback import play
+
+# To play audio text-to-speech during execution
+def tts_speak(my_text):
+    with BytesIO() as f:
+        gTTS(text=my_text, lang='en').write_to_fp(f)
+        f.seek(0)
+        song = AudioSegment.from_file(f, format="mp3")
+        play(song)
+
+tts_speak("Why are you the way you are?")
